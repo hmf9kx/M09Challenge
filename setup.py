f9cb0881d32cb718149ff44ba33f3ac166c168f3
accrlrr <accrlrr@gmail.com>
2023-07-31 10:52:52 -0400

    update

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='mean',
       version='0.1',
       description='Calculate mean of list',
+      url='https://github.com/hmf9kx/M09Challenge',
       author='Hayden French',
       author_email='hmf9kx@virginia.edu',
       license='MIT',
